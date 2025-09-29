e44d9cc625c6528c16ba9ba1b038ef9e3aed556b
vishwasuresh001 <vishwa012rr@gmail.com>
2025-09-29 19:08:39 +0530

    Success

---
+++ Vish/Vish/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from mathapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('bmi/',views.calculate_bmi,name="bmi"),
+    path('',views.calculate_bmi,name="bmicalculator")
+]
---
+++ Vish/mathapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+def calculate_bmi(request):
+    context={}
+    context['bmi']="0"
+    context['w']="0"
+    context['h']="0"
+    if(request.method=='POST'):
+       w= float(request.POST.get('weight','0'))
+       h=float(request.POST.get('height','0'))
+       print('request=',request)
+       
+       print('Weight=',w)
+       print('Height=',h)
+       bmi=w/((h/100)**2)
+       context['bmi']=bmi
+       context['w']=w
+       context['h']=h
+       print('BMI=',bmi)
+    return render(request,'mathapp/vijay.html',context)
